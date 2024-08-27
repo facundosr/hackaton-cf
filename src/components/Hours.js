04c491e2364de8f7569757a529905680b51ccac6
Facundo <73291291+facundosr@users.noreply.github.com>
2024-08-27 19:27:28 +0000

    CREATE Hours,js

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,26 @@
-// Placeholder to host a component
+import React from 'react';
+
+const OpeningHours = () => {
+  const hours = [
+    { day: "Monday", open: "10:00", close: "16:00" },
+    { day: "Tuesday", open: "10:00", close: "16:00" },
+    { day: "Wednesday", open: "10:00", close: "16:00" },
+    { day: "Thursday", open: "10:00", close: "16:00" },
+    { day: "Friday", open: "10:00", close: "16:00" },
+    { day: "Saturday", open: "9:00", close: "20:00" },
+    { day: "Sunday", open: "9:00", close: "20:00" },
+  ];
+
+  const today = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+
+  const todayHour = hours.find((day) => day.day === today);
+
+  return (
+     <div id="hours">
+        <h2>Today's Hours</h2>
+        <p>{todayHour.day} {todayHour.open} - {todayHour.close}</p>
+    </div>
+  );
+};
+
+export default OpeningHours;
